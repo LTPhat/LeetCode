379fd4adc71623f2576d57c2dd50ca123d0d29ce
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-06-17 19:04:28 +0700

    17/06/23

---
+++ C++/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,39 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        std::unordered_map<int, int> hashmap;
+        for (int i = 0; i < arr.size(); i++) {
+            if (hashmap.count(arr[i]))
+                hashmap[arr[i]] += 1;
+            else
+                hashmap[arr[i]] = 1;
+        }
+        
+        std::unordered_set<int> valueSet;
+        for (const auto& pair : hashmap) {
+            if (valueSet.count(pair.second))
+                return false;
+            valueSet.insert(pair.second);
+        }
+        
+        return true;
+    }
+};
---
+++ Javascript/1207. Unique Number of Occurrences.js
@@ -0,0 +1,33 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const hashmap = {}
+    for (let i = 0; i < arr.length; i++){
+        if (hashmap[arr[i]]) hashmap[arr[i]] += 1
+        else
+            hashmap[arr[i]] = 1
+    }
+    const values = Object.values(hashmap)
+    const set = new Set(values)
+    return set.size === values.length
+};
