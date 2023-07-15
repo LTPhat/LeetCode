7fed972fa3366a1088ec72a4ee076f9743efad10
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-07-15 17:37:08 +0700

    15/07/23

---
+++ Javascript/122. Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,40 @@
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+// Example 2:
+
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// Total profit is 4.
+// Example 3:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let afterBuy = - Number.MAX_SAFE_INTEGER
+    let afterSell = 0
+    for(let i = 0; i < prices.length; i++){
+        afterBuy = Math.max(afterBuy, afterSell - prices[i])
+        afterSell = Math.max(afterSell, afterBuy + prices[i])
+    }
+    return afterSell > 0? afterSell:0
+};
