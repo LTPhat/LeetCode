2895916a87f9094b721335b2407384e5cbcbb01e
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-07-26 22:45:01 +0700

    26/07/23

---
+++ C++/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,48 @@
+// An array arr a mountain if the following properties hold:
+
+// arr.length >= 3
+// There exists some i with 0 < i < arr.length - 1 such that:
+// arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+// arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+// Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+// You must solve it in O(log(arr.length)) time complexity.
+
+ 
+
+// Example 1:
+
+// Input: arr = [0,1,0]
+// Output: 1
+// Example 2:
+
+// Input: arr = [0,2,1,0]
+// Output: 1
+// Example 3:
+
+// Input: arr = [0,10,5,2]
+// Output: 1
+
+
+#include <vector>
+#include <iostream>
+
+int peakIndexInMountainArray(std::vector<int>& arr) {
+    int left = 0;
+    int right = arr.size() - 1;
+    int mid;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        } else if (arr[mid - 1] > arr[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+}
