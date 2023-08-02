61b5878f45199943d0e204af79cfd9545070c524
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-08-02 09:57:37 +0700

    02/08/23

---
+++ Python/46. Permutations.py
@@ -0,0 +1,33 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(nums, path): 
+            if not nums: 
+                result.append(path) 
+                return 
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], path + [nums[i]]) 
+        result = [] 
+        backtrack(nums, []) 
+        return result 
