5517cd5fcbb3346fc46c7aca2ab5cb2d55e45110
LTPhat <lamthanhphat0918398911@gmail.com>
2023-11-18 15:41:53 +0700

    18/11/23

---
+++ Javascript/1838. Frequency of the Most Frequent Element.js
@@ -0,0 +1,46 @@
+// The frequency of an element is the number of times it occurs in an array.
+
+// You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
+
+// Return the maximum possible frequency of an element after performing at most k operations.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,4], k = 5
+// Output: 3
+// Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+// 4 has a frequency of 3.
+// Example 2:
+
+// Input: nums = [1,4,8,13], k = 5
+// Output: 2
+// Explanation: There are multiple optimal solutions:
+// - Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+// - Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+// - Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function(nums, k) {
+    nums.sort((a, b) => a - b);
+        let left = 0;
+        let prevSum = 0;
+        let afterSum = 0;
+
+        for (let right = 0; right < nums.length; right++) {
+            prevSum += nums[right];
+            afterSum = (right - left + 1) * nums[right];
+
+            if (afterSum - prevSum > k) {
+                prevSum -= nums[left];
+                left += 1;
+            }
+        }
+
+        return nums.length - left;
+};
