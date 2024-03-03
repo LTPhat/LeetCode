399dd27096c234892a4dbb0dee65bcd01dffbffd
Lam Thanh Phat <lamthanhphat0918398911@gmail.com>
2024-03-03 10:52:49 +0700

    03/03/24

---
+++ C++/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,52 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        for (int i = 0; i < n; ++i) {
+            fast = fast->next;
+        }
+
+        if (!fast) {
+            head = head->next;
+            return head;
+        }
+
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return head;
+    }
+};
---
+++ Python/19. Remove Nth Node From End of List.py
@@ -0,0 +1,49 @@
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+# Example 2:
+
+# Input: head = [1], n = 1
+# Output: []
+# Example 3:
+
+# Input: head = [1,2], n = 1
+# Output: [1]
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+
+        for i in range(n):
+            fast = fast.next
+
+        
+        if not fast:
+            head = head.next
+            return head
+        
+        while (fast.next):
+            fast = fast.next
+            slow = slow.next
+
+
+        slow.next = slow.next.next
+
+        return head
+        
