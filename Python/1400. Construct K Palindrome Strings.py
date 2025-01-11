73040539df04d84369b43cd0f4cce36daefcd6ec
Lam Thanh Phat <lamthanhphat0918398911@gmail.com>
2025-01-11 09:02:36 +0700

    11/01/25

---
+++ Python/1400. Construct K Palindrome Strings.py
@@ -0,0 +1,49 @@
+# Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "annabelle", k = 2
+# Output: true
+# Explanation: You can construct two palindromes using all characters in s.
+# Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+# Example 2:
+
+# Input: s = "leetcode", k = 3
+# Output: false
+# Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+# Example 3:
+
+# Input: s = "true", k = 4
+# Output: true
+# Explanation: The only possible solution is to put each character in a separate string.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists of lowercase English letters.
+# 1 <= k <= 105
+
+
+class Solution(object):
+    def canConstruct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        if len(s) < k: 
+            return False
+        fre_map = {}
+        for char in s:
+            fre_map[char] = fre_map.get(char, 0) + 1
+        count = 0
+        for char in fre_map.keys():
+            if fre_map[char] % 2 != 0:
+                count += 1
+            if count > k:
+                return False
+        
+        return True
