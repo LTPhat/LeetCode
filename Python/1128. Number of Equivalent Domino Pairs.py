0a5778786cd1beb67f7d759dd45f2b09eff87f68
Lam Thanh Phat <{102889533}+{LTPhat}@users.noreply.github.com>
2025-05-04 20:08:25 +0700

    4/5/25

---
+++ Python/1128. Number of Equivalent Domino Pairs.py
@@ -0,0 +1,44 @@
+# Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+
+# Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+
+ 
+
+# Example 1:
+
+# Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+# Output: 1
+# Example 2:
+
+# Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+# Output: 3
+#  Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+
+# Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+
+ 
+
+# Example 1:
+
+# Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+# Output: 1
+# Example 2:
+
+# Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+# Output: 3
+ 
+
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        """
+        :type dominoes: List[List[int]]
+        :rtype: int
+        """
+        count = 0
+        seen_pairs = defaultdict(int)
+        for a, b in dominoes:
+            key = (min(a, b), max(a, b))
+            count += seen_pairs[key]
+            seen_pairs[key] += 1
+
+        return count
